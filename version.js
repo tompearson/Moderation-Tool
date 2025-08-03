8c731362257dac2291dc8eccd012629405238d5d
Tom Pearson <tom_pearson@yahoo.com>
2025-08-03 11:59:31 -0700

    chore: final version 0.8.15-alpha

--- version.js
+++ version.js
@@ -2,12 +2,12 @@
 // This file serves as the single source of truth for version information
 
 const VERSION = {
-  number: '0.8.14-alpha',
+  number: '0.8.16-alpha',
   major: 0,
   minor: 8,
-  patch: 14,
+  patch: 16,
   prerelease: 'alpha',
-  full: '0.8.14-alpha'
+  full: '0.8.16-alpha'
 };
 
 // Export for CommonJS (Node.js)
