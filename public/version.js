6b9607e620eb72b0421de43c3e77645624eca20d
Tom Pearson <tom_pearson@yahoo.com>
2025-08-03 14:12:07 -0700

    Final version update to 0.8.30-alpha (pre-commit hook removed)

--- public/version.js
+++ public/version.js
@@ -2,12 +2,12 @@
 // This file serves as the single source of truth for version information
 
 const VERSION = {
-  number: '0.8.29-alpha',
+  number: '0.8.30-alpha',
   major: 0,
   minor: 8,
-  patch: 29,
+  patch: 30,
   prerelease: 'alpha',
-  full: '0.8.29-alpha'
+  full: '0.8.30-alpha'
 };
 
 // Export for CommonJS (Node.js)
