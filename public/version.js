0ac9678c11cd11200c3fd0af3ebec6238cb8aeac
Tom Pearson <tom_pearson@yahoo.com>
2025-08-03 14:09:42 -0700

    Update version to 0.8.28-alpha

--- public/version.js
+++ public/version.js
@@ -2,12 +2,12 @@
 // This file serves as the single source of truth for version information
 
 const VERSION = {
-  number: '0.8.27-alpha',
+  number: '0.8.28-alpha',
   major: 0,
   minor: 8,
-  patch: 27,
+  patch: 28,
   prerelease: 'alpha',
-  full: '0.8.27-alpha'
+  full: '0.8.28-alpha'
 };
 
 // Export for CommonJS (Node.js)
